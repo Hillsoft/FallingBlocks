12596f594a58385165ae0c88d9670b616c7327b2
Matthew Hillman <mhillman2@aol.com>
2025-04-02 19:19:46 +0100

    Move render pass logic from `VulkanCommandBuffer` to `RenderSubSystem`

--- src/render/RenderSubSystem.cpp
+++ src/render/RenderSubSystem.cpp
@@ -220,26 +220,73 @@ void RenderSubSystem::drawWindow(size_t windowId) {
           renderables_[command.obj_.id].has_value());
       RenderableQuad& renderable = *renderables_[command.obj_.id];
 
-      commandBuffers_[currentFrame_].runRenderPass(
-          renderable.pipeline_,
-          presentFrame.getFrameBuffer(),
-          window.presentStack_.extent(),
-          [&](VkCommandBuffer buffer) {
-            vkCmdBindDescriptorSets(
-                buffer,
-                VK_PIPELINE_BIND_POINT_GRAPHICS,
-                renderable.pipeline_.getPipelineLayout().getRawLayout(),
-                0,
-                1,
-                &renderable.descriptorPool_.getDescriptorSets()[currentFrame_],
-                0,
-                nullptr);
+      VkCommandBuffer commandBuffer =
+          commandBuffers_[currentFrame_].getRawBuffer();
+      vkResetCommandBuffer(commandBuffer, 0);
 
-            VkBuffer vertexBuffer = renderable.vertexAttributes_.getRawBuffer();
-            VkDeviceSize offsets = 0;
-            vkCmdBindVertexBuffers(buffer, 0, 1, &vertexBuffer, &offsets);
-            vkCmdDraw(buffer, 6, 1, 0, 0);
-          });
+      VkCommandBufferBeginInfo beginInfo{};
+      beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+      beginInfo.flags = 0;
+      beginInfo.pInheritanceInfo = nullptr;
+
+      if (vkBeginCommandBuffer(commandBuffer, &beginInfo) != VK_SUCCESS) {
+        throw std::runtime_error{"Failed to begin recording command buffer"};
+      }
+
+      VkRenderPassBeginInfo renderPassInfo{};
+      renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+      renderPassInfo.renderPass = mainRenderPass_.get();
+      renderPassInfo.framebuffer = presentFrame.getFrameBuffer();
+      renderPassInfo.renderArea.offset = {0, 0};
+      renderPassInfo.renderArea.extent = window.presentStack_.extent();
+
+      VkClearValue clearColor = {{{0.0f, 0.0f, 0.0f, 1.0f}}};
+      renderPassInfo.clearValueCount = 1;
+      renderPassInfo.pClearValues = &clearColor;
+
+      vkCmdBeginRenderPass(
+          commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+      vkCmdBindPipeline(
+          commandBuffer,
+          VK_PIPELINE_BIND_POINT_GRAPHICS,
+          renderable.pipeline_.getRawPipeline());
+
+      VkViewport viewport{};
+      viewport.x = 0.0f;
+      viewport.y = 0.0f;
+      viewport.width = static_cast<float>(window.presentStack_.extent().width);
+      viewport.height =
+          static_cast<float>(window.presentStack_.extent().height);
+      viewport.minDepth = 0.0f;
+      viewport.maxDepth = 1.0f;
+      vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
+
+      VkRect2D scissor{};
+      scissor.offset = {0, 0};
+      scissor.extent = window.presentStack_.extent();
+      vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
+
+      vkCmdBindDescriptorSets(
+          commandBuffer,
+          VK_PIPELINE_BIND_POINT_GRAPHICS,
+          renderable.pipeline_.getPipelineLayout().getRawLayout(),
+          0,
+          1,
+          &renderable.descriptorPool_.getDescriptorSets()[currentFrame_],
+          0,
+          nullptr);
+
+      VkBuffer vertexBuffer = renderable.vertexAttributes_.getRawBuffer();
+      VkDeviceSize offsets = 0;
+      vkCmdBindVertexBuffers(commandBuffer, 0, 1, &vertexBuffer, &offsets);
+      vkCmdDraw(commandBuffer, 6, 1, 0, 0);
+
+      vkCmdEndRenderPass(commandBuffer);
+
+      if (vkEndCommandBuffer(commandBuffer) != VK_SUCCESS) {
+        throw std::runtime_error{"Failed to record command buffer"};
+      }
     }
   }
 
--- src/render/VulkanCommandBuffer.hpp
+++ src/render/VulkanCommandBuffer.hpp
@@ -1,13 +1,10 @@
 #pragma once
 
-#include <stdexcept>
 #include <vector>
 #include <GLFW/glfw3.h>
 
 #include "render/VulkanCommandPool.hpp"
 #include "render/VulkanGraphicsDevice.hpp"
-#include "render/VulkanGraphicsPipeline.hpp"
-#include "render/VulkanRenderPass.hpp"
 #include "render/vulkan/UniqueHandle.hpp"
 
 namespace blocks::render {
@@ -16,13 +13,6 @@ class VulkanCommandBuffer {
  public:
   VulkanCommandBuffer(
       VulkanGraphicsDevice& device, VulkanCommandPool& commandPool);
-
-  template <typename Fn>
-  void runRenderPass(
-      VulkanGraphicsPipeline& pipeline,
-      VkFramebuffer frameBuffer,
-      VkExtent2D extent,
-      Fn&& fn);
 
   void submit(
       const std::vector<VkSemaphore>& waitSemaphores,
@@ -35,65 +25,5 @@ class VulkanCommandBuffer {
   vulkan::UniqueHandle<VkCommandBuffer> commandBuffer_;
   VkQueue queue_;
 };
-
-template <typename Fn>
-void VulkanCommandBuffer::runRenderPass(
-    VulkanGraphicsPipeline& graphicsPipeline,
-    VkFramebuffer frameBuffer,
-    VkExtent2D extent,
-    Fn&& fn) {
-  vkResetCommandBuffer(commandBuffer_.get(), 0);
-
-  VkCommandBufferBeginInfo beginInfo{};
-  beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-  beginInfo.flags = 0;
-  beginInfo.pInheritanceInfo = nullptr;
-
-  if (vkBeginCommandBuffer(commandBuffer_.get(), &beginInfo) != VK_SUCCESS) {
-    throw std::runtime_error{"Failed to begin recording command buffer"};
-  }
-
-  VkRenderPassBeginInfo renderPassInfo{};
-  renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-  renderPassInfo.renderPass =
-      graphicsPipeline.getRenderPass().getRawRenderPass();
-  renderPassInfo.framebuffer = frameBuffer;
-  renderPassInfo.renderArea.offset = {0, 0};
-  renderPassInfo.renderArea.extent = extent;
-
-  VkClearValue clearColor = {{{0.0f, 0.0f, 0.0f, 1.0f}}};
-  renderPassInfo.clearValueCount = 1;
-  renderPassInfo.pClearValues = &clearColor;
-
-  vkCmdBeginRenderPass(
-      commandBuffer_.get(), &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
-
-  vkCmdBindPipeline(
-      commandBuffer_.get(),
-      VK_PIPELINE_BIND_POINT_GRAPHICS,
-      graphicsPipeline.getRawPipeline());
-
-  VkViewport viewport{};
-  viewport.x = 0.0f;
-  viewport.y = 0.0f;
-  viewport.width = static_cast<float>(extent.width);
-  viewport.height = static_cast<float>(extent.height);
-  viewport.minDepth = 0.0f;
-  viewport.maxDepth = 1.0f;
-  vkCmdSetViewport(commandBuffer_.get(), 0, 1, &viewport);
-
-  VkRect2D scissor{};
-  scissor.offset = {0, 0};
-  scissor.extent = extent;
-  vkCmdSetScissor(commandBuffer_.get(), 0, 1, &scissor);
-
-  fn(commandBuffer_.get());
-
-  vkCmdEndRenderPass(commandBuffer_.get());
-
-  if (vkEndCommandBuffer(commandBuffer_.get()) != VK_SUCCESS) {
-    throw std::runtime_error{"Failed to record command buffer"};
-  }
-}
 
 } // namespace blocks::render
