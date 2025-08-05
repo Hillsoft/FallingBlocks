a483ee3f0cdb2b4b66c095d3b67e2bec1e18fe16
Matthew Hillman <mhillman2@aol.com>
2025-08-05 21:13:36 +0100

    Add intersection test for Bezier curves

--- src/shaders/fontFragment.glsl
+++ src/shaders/fontFragment.glsl
@@ -30,7 +30,37 @@ int intersectsStraight(ivec2 a, ivec2 b) {
 }
 
 int intersectsQuadratic(ivec2 a, ivec2 handle, ivec2 b) {
-  return intersectsStraight(a, b);
+  float qa = a.y - 2 * handle.y + b.y;
+  float qb = 2 * handle.y - 2 * a.y;
+  float qc = a.y - uv.y;
+
+  float discriminant = qb*qb - 4 * qa * qc;
+
+  if (discriminant < 0) {
+    return 0;
+  }
+
+  int windingNumber = 0;
+
+  float t0 = (-qb + sqrt(discriminant)) / (2 * qa);
+  if (0 < t0 && t0 < 1) {
+    float x0 = (1.0 - t0) * (1.0 - t0) * a.x + 2 * t0 * (1.0 - t0) * handle.x + t0 * t0 * b.x;
+	if (x0 > uv.x) {
+	  float dy = 2 * b.y * t0 - 2 * handle.y * t0 + 2 * handle.y * (1.0 - t0) - 2 * a.y * (1.0 - t0);
+	  windingNumber += dy > 0 ? 1 : -1;
+	}
+  }
+
+  float t1 = (-qb - sqrt(discriminant)) / (2 * qa);
+  if (0 < t1 && t1 < 1) {
+    float x1 = (1.0 - t1) * (1.0 - t1) * a.x + 2 * t1 * (1.0 - t1) * handle.x + t1 * t1 * b.x;
+	if (x1 > uv.x) {
+	  float dy = 2 * b.y * t1 - 2 * handle.y * t1 + 2 * handle.y * (1.0 - t1) - 2 * a.y * (1.0 - t1);
+	  windingNumber += dy > 0 ? 1 : -1;
+	}
+  }
+
+  return windingNumber;
 }
 
 void main() {
